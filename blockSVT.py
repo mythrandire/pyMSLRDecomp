ac790157819078b7448fe80a08b4b7693bc2e171
mythrandire <39613187+mythrandire@users.noreply.github.com>
2020-05-03 18:05:59 -0400

    buildback created, doesn't work - check

--- blockSVT.py
+++ blockSVT.py
@@ -10,7 +10,6 @@ def makeblocks(X, block_size):
     the given block size
     """
 
-
     rows, cols = X.shape
 
     nrows = block_size[0]
@@ -19,6 +18,33 @@ def makeblocks(X, block_size):
     list = [x for x in temp]
     return list
 
+
+def buildback(list, block_size, og_size):
+    """
+    Inverse of function 'makeblocks'.
+    Reorganizes processed block-wise data into a matrix of shape equal to
+    the original matrix given to 'makeblocks'.
+    Inputs:
+        list            :   list of blocks from 'makeblock'
+        block_size      :   shape of the blocks in the list
+        og_size         :   Original shape of input array, the size which
+                            the returned array will be reshaped to.
+    Returns:
+        data            :   data array reshaped to desired shape
+
+    Note: block_size passed explicitly to avoid confusion, function can be
+    rewritten to infer the block_size by checking list[i].shape
+
+    """
+
+    nrows = block_size[0]
+    ncols = block_size[1]
+    nblocks = og_size[0]//nrows
+    arr = np.array(list).reshape(nblocks//2**(nrows-1), nblocks//2**(ncols-1), nrows, ncols)
+    arr = arr.swapaxes(1, 2)
+    data = arr.reshape(og_size[0], og_size[1])
+
+    return data
 
 
 def blockSVT(Z, block_size, lambd):
@@ -31,7 +57,8 @@ def blockSVT(Z, block_size, lambd):
     Returns:
         Z           :       Block-wise thresholded
     """
-    eps = np.finfo(float).eps
+
+    eps = np.finfo(float).eps # machine epsilon
     doBlockSVT = lambda X: SVT(X, lambd)
     if block_size[0] == Z.size:
         t = np.norm(Z.flatten(), 2)
@@ -42,13 +69,14 @@ def blockSVT(Z, block_size, lambd):
         # output_list = [function(x) for x in input_list]
         #
         # data = I.extract_patches_2d(Z, block_size, int(Z.shape[0]/block_size[0]))
+        Z_shape = Z.shape
         data = makeblocks(Z, block_size)
         print("block data shape: ", len(data))
+        print(data[0])
         #print(data)
         outdata = [doBlockSVT(x) for x in data]
         #temp = doBlockSVT(data)
-        print("Size of BSVT output: ", len(outdata))
-
-
+        print("Size of BSVT output: ", np.size(outdata))
+        Z = buildback(outdata, block_size, Z_shape)
 
     return Z
