7cd21de9d949aa6131f83ac4e9686268ef2611fd
Dwiref Oza <mithrandir.dso@gmail.com>
2020-05-09 18:13:01 -0400

    parallel pools boi

--- liveplot.py
+++ liveplot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 
 
-def liveplot(X):
+def liveplot(X, iter):
     """
     Description
     Inputs:
@@ -17,7 +17,6 @@ def liveplot(X):
     #nrows, ncols = grid_shape
 
     X_new = np.concatenate((X[:,:,0], X[:,:,1], X[:,:,2], X[:,:,3]), axis=1)
-    # X_new = np.concatenate((X[:,:,0], X[:,:,1]), axis=1)
-    #norm = Normalize(vmin=0, vmax=255, clip=False)
-    plt.imshow(X_new), plt.title('Decomposition')
-    plt.show()
+    plt.imshow(X_new), plt.title('Decomposition: Iteration %i' %iter)
+    plt.draw()
+    plt.pause(0.01)
--- pool_decomp.py
+++ pool_decomp.py
@@ -1,4 +1,5 @@
 import numpy as np
+from multiprocessing import Pool
 import numpy.matlib as npm
 import matplotlib.pyplot as plt
 import cv2
@@ -8,63 +9,52 @@ from liveplot import liveplot
 from matplotlib.colors import Normalize
 from skimage.transform import resize
 
-
-N = 64
+nIter = 100
+rho = 10
+X = cv2.imread('./fundus.png')[:,:,0]
+X = resize(X, (256, 256))
+N = 256
 L = np.log2(N)
 FOV = (N, N)
-
-sigma = 0
-
-nIter = 100
-
-rho = 10 # ADMM param
-
 max_L = L
-
 block_sizes = 2**np.arange(0, int(max_L+1),2)
 ms = block_sizes
 ns = ms
 block_sizes = [(x, x) for x in block_sizes]
-
 print("Block sizes: ", block_sizes)
-
 levels = len(block_sizes)
-
 bs = np.prod(np.divide(npm.repmat(FOV, levels, 1), block_sizes), 1).astype(np.int)
 print("bs is: ", bs)
 lambdas = np.sqrt(ms) + np.sqrt(ns) + np.sqrt(np.log2(np.multiply(bs, np.minimum(ms, ns))))
-# ms = ns for square input so don't need np.minimum here strictly speaking
-
-
-X = cv2.imread('./fundus.png')[:,:,0]
-print(X.shape)
-X = resize(X, (64, 64))
-# X = np.ones((4, 4))
-#X = np.array([[1, 4, 8, 1], [2, 4, 6, 1], [8, 7, 2, 4], [7, 5, 3, 9]])
-print(X)
 FOVl = FOV + (levels,)
-
 level_dim = len(FOV)
-plt.imshow(X), plt.show()
 
 A = lambda x : np.sum(x, level_dim) # Summation
-
 AT = lambda x : np.repeat(x[:,:,np.newaxis], levels, axis=2) # Adjoint Operator
 
 X_it = np.zeros(FOVl)
 Z_it = np.zeros(FOVl)
 U_it = np.zeros(FOVl)
 
+def parSVT(l):
+    """
+    """
+    X_it_f = np.transpose(X_it, (2, 0, 1))
+    U_it_f = np.transpose(U_it, (2, 0, 1))
+    out = blockSVT((X_it_f[l,:,:] + U_it_f[l,:,:]), block_sizes[l], (lambdas[l] / rho))
+    return out
+
+numpools = levels if levels<=10 else 10
 
 for it in range(nIter):
     X_it = 1 / levels * AT(X - A(Z_it - U_it)) + Z_it - U_it
-    for l in range(levels):
-        Z_it[:,:,l] = blockSVT((X_it[:,:,l] + U_it[:,:,l]), block_sizes[l], (lambdas[l] / rho))
-
-
-
+    with Pool(processes=numpools) as pool:
+        data = pool.map(parSVT, range(levels))
+        pool.close()
+        pool.terminate()
+        pool.join()
+    print('pool output length: ', len(data))
+    Z_it = np.reshape(np.asarray(data), (levels, N, N))
+    Z_it = np.transpose(Z_it, (1, 2, 0))
     U_it = U_it - Z_it + X_it
-
-
-liveplot(np.abs(X_it))
-#print(X_it[:,:,0] == X_it[:,:,1])
+    liveplot(np.abs(X_it), it)
